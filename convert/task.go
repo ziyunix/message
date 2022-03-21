edda5951a64a50fba91491fa4817c924fa7dec06
张宗良 <storezhang@qq.com>
2022-03-21 09:54:39 +0800

    drone

---
+++ convert/task.go
@@ -0,0 +1,27 @@
+package convert
+
+import (
+	`path/filepath`
+
+	`github.com/goexl/gfx`
+	`github.com/storezhang/transfer`
+	`github.com/ziyunix/core`
+)
+
+var _ = NewTask
+
+// NewTask 创建转码任务
+func NewTask(id int64, file *transfer.File, converts ...*core.Convert) *Task {
+	return &Task{
+		Id:       id,
+		File:     file,
+		Converts: converts,
+	}
+}
+
+func (t *Task) SrcFilename(home string) string {
+	return filepath.Join(t.SrcDir(home), filepath.Base(t.File.Name))
+}
+func (t *Task) SrcDir(home string) string {
+	return filepath.Join(home, `src`, gfx.Filename(t.File.Name))
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ziyunix/message
+
+go 1.17
+
+require (
+	github.com/goexl/gfx v0.0.3
+	github.com/storezhang/transfer v0.0.3
+	github.com/ziyunix/core v0.0.2
+)
